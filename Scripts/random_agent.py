84b6797fd121bd5506a40ada268fa10001a6c2aa
aleksandr <aleksandr.myashin@gmail.com>
2019-02-28 19:54:06 +0700

    rewrite RandomAgent

--- Scripts/random_agent.py
+++ Scripts/random_agent.py
@@ -3,6 +3,8 @@ import sys
 
 import gym
 from gym import wrappers, logger
+from GymRunner import GymRunner
+import numpy as np
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -10,7 +12,7 @@ class RandomAgent(object):
         self.action_space = action_space
 
     def act(self, observation, reward, done):
-        return self.action_space.sample()
+        return  np.random.randint(self.action_space)
 
 # New comment
 if __name__ == '__main__':
@@ -22,31 +24,10 @@ if __name__ == '__main__':
     # want to change the amount of output.
     logger.set_level(logger.INFO)
     
-    env = gym.make(args.env_id)
+ 
+ 
+    GR = GymRunner("LunarLander-v2", '/tmp/random-agent-results') 
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
-    outdir = '/tmp/random-agent-results'
-    env = wrappers.Monitor(env, directory=outdir, force=True)
-    env.seed(0)
-    agent = RandomAgent(env.action_space)
+    agent = RandomAgent(GR.get_shapes()["action_space"])
 
-    episode_count = 100
-    reward = 0
-    done = False
-
-    for i in range(episode_count):
-        ob = env.reset()
-        while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            if done:
-                break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-
-    # Close the env and write monitor result info to disk
-    env.close()
+    GR.test_agent(agent, 1000000000)
