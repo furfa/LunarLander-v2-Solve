f5a8093a13154dc8e5fe7035b882810e6eab0eef
aleksandr <aleksandr.myashin@gmail.com>
2019-03-06 15:51:44 +0700

    Добавил включение и отключение визуализации

--- Scripts/GymRunner.py
+++ Scripts/GymRunner.py
@@ -44,39 +44,58 @@ class GymRunner():
                 observation = next_observation
 
     def fit(self, agent, n_iters, max_iters=2000, batch_size=32, LEARN_FREQ=1, visualize=True):
-
         env = self.env
         scores = list()
-
         pbar = tqdm( range(n_iters) )
-        for iteration_num in pbar:
-            done = False
-            observation = env.reset()
-            score = 0
-            for i in range(max_iters):
-                if visualize:
-                    env.render()
-                action = agent.chooseAction( observation=observation)
 
-                next_observation, reward, done, info = env.step(action)
-                score += reward
+        def main_loop(env, scores, pbar, visualize):
 
-                agent.memory.append(observation, action, reward, next_observation, done)
+            for iteration_num in pbar:
+                done = False
+                observation = env.reset()
+                score = 0
+                for i in range(max_iters):
+                    if visualize:
+                        env.render()
+                    action = agent.chooseAction( observation=observation)
 
-                observation = next_observation            
-                if i % LEARN_FREQ == 0:
-                    agent.learn(batch_size)
-                if done:
-                    break
+                    next_observation, reward, done, info = env.step(action)
+                    score += reward
 
-            scores.append(score)
-            pbar.set_description(f"[SCORE] {score:.2f}")
-            if iteration_num % 100 == 0 and iteration_num!=0:
-                scores = scores[-100:]
-                print(
-                    f"\n[MEAN_SCORE] {np.mean(scores):.2f} [STD_SCORE] {np.std(scores):.2f} \n", 
-                    end=""
-                )
+                    agent.memory.append(observation, action, reward, next_observation, done)
+
+                    observation = next_observation            
+                    if i % LEARN_FREQ == 0:
+                        agent.learn(batch_size)
+                    if done:
+                        break
+
+                scores.append(score)
+                pbar.set_description(f"[SCORE] {score:.2f}")
+                if iteration_num % 100 == 0 and iteration_num!=0:
+                    scores = scores[-100:]
+                    print(
+                        f"\n[MEAN_SCORE] {np.mean(scores):.2f} [STD_SCORE] {np.std(scores):.2f} \n", 
+                        end=""
+                    )
+                    
+        def try_block(env, scores, pbar, visualize):
+            try:
+                main_loop(env, scores, pbar, visualize)
+            except KeyboardInterrupt:
+                inp = input("""
+                    o - Остановка обучения,
+                    +v - Включить визуализацию
+                    -v - Выключить визуализацию
+                """)
+                if inp == 'o':
+                    print("Остановка обучения.")
+                elif inp == '+v':
+                    try_block(env, scores, pbar, True)
+                elif inp == '-v':
+                    try_block(env, scores, pbar, False)
+
+        try_block(env, scores, pbar, visualize)
 
     def test_agent(self, agent, n_iters):
         self.env = gym.make(self.ENV_NAME)
