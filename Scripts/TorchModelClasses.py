873cf8c0eb9621205c3e901a2ab346526787b3bc
aleksandr <aleksandr.myashin@gmail.com>
2019-03-11 17:04:35 +0700

    Huber Model

--- Scripts/TorchModelClasses.py
+++ Scripts/TorchModelClasses.py
@@ -33,3 +33,29 @@ class SimpleNet(nn.Module):
         actions = self.model(observation)
         return actions
 
+
+class HuberNet(nn.Module):
+    def __init__(self, ALPHA, INPUT_SHAPE, OUTPUT_SHAPE):
+        super().__init__()
+
+        self.ALPHA = ALPHA
+
+        self.model = nn.Sequential(
+            nn.Linear(INPUT_SHAPE, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, OUTPUT_SHAPE),
+        )
+
+        self.optimizer = optim.RMSprop(self.parameters(), lr=self.ALPHA, momentum=0.0001) # Tune this
+
+        self.loss = nn.SmoothL1Loss()
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')     
+        self.to(self.device)
+
+    def forward(self, observation):
+        observation = torch.Tensor(observation).to(self.device)
+        actions = self.model(observation)
+        return actions
+
